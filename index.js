56c52be0aa5026b8ce7d2401b748f22e1a1e5a76
Keenan Lee-Peters <keenan.leepeters@gmail.com>
2022-06-28 16:39:12 -0600

    blog in models is complete and connected to mongoDB

--- index.js
+++ index.js
@@ -2,29 +2,11 @@
 //const app = require('./app')
 const config = require('./utils/config')
 const http = require('http')
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
+const app = require('./app')
 const logger = require('./utils/logger')
 
 const server = http.createServer(app)
 
-const blogSchema = new mongoose.Schema({
-    title: String,
-    author: String,
-    url: String,
-    likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-const mongoUrl = 'mongodb+srv://fullstack:cattle4390equal@cluster0.l3wi6dm.mongodb.net/bloglist?retryWrites=true&w=majority'
-mongoose.connect(mongoUrl)
-
-app.use(cors())
-app.use(express.json())
 
 server.listen(config.PORT, () => {
     logger.info(`Server running on port ${config.PORT}`)
