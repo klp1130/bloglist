8ffb876e16463826936c681940574d35f132be9e
Keenan Lee-Peters <keenan.leepeters@gmail.com>
2022-06-27 18:47:08 -0600

    added logger file updated index global variables

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+
+//const app = require('./app.js')
 const http = require('http')
 require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
+const logger = require('./utils/logger')
 
 const blogSchema = new mongoose.Schema({
   title: String,
@@ -40,5 +43,5 @@ app.post('/api/blogs', (request, response) => {
 
 const PORT = 3003
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+  logger.info(`Server running on port ${PORT}`)
 })
