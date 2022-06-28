4b135497ad7f4e31c9b290c97fa9a5e7e8c05b5e
Keenan Lee-Peters <keenan.leepeters@gmail.com>
2022-06-27 19:06:59 -0600

    changed linter to use 4 spaces not 2 and added config and models and controllers

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 
-//const app = require('./app.js')
+//const app = require('./app')
+const config = require('./utils/config')
 const http = require('http')
 require('dotenv').config()
 const express = require('express')
@@ -8,11 +9,13 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const logger = require('./utils/logger')
 
+const server = http.createServer(app)
+
 const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
+    title: String,
+    author: String,
+    url: String,
+    likes: Number
 })
 
 const Blog = mongoose.model('Blog', blogSchema)
@@ -23,25 +26,6 @@ mongoose.connect(mongoUrl)
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-const PORT = 3003
-app.listen(PORT, () => {
-  logger.info(`Server running on port ${PORT}`)
+server.listen(config.PORT, () => {
+    logger.info(`Server running on port ${config.PORT}`)
 })
