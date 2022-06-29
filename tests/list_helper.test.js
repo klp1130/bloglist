4309f374ed0de5d33b7afe3c227fa26ada5ee36f
Keenan Lee-Peters <keenan.leepeters@gmail.com>
2022-06-29 11:55:35 -0600

    added total likes function to lest helper in utils

--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -1,10 +1,9 @@
 const listHelper = require('../utils/list_helper')
 
 // Dummy
-test('dummy returns on', () => {
+test('dummy returns one', () => {
     const blogs = []
 
     const result = listHelper.dummy(blogs)
     expect(result).toBe(1)
 })
-
