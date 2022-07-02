0fdd5202e13e7aec69a5189655680a8777208ac6
Keenan Lee-Peters <96672628+klp1130@users.noreply.github.com>
2022-07-02 08:15:31 -0600

    logic for author with the most blogs added with logic for author with most likes

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,6 +1,8 @@
 //Helper functions go here
 
 const blog = require("../models/blog")
+const _ = require('lodash')
+
 
 const dummy = (blogs) => {
     //..
@@ -22,10 +24,31 @@ const favoriteBlog = (blogs) => {
     return blog
 
 }
+/* returns the author who has the largest amount of blogs. it also contains
+the number of blogs the top author has */
+
+const mostBlogs = (blogs) => {
+    const bMap = _.countBy(blogs, (blog) => blog.author)
+    const authorBlogCount = _.keys(bMap).map(author => {
+        return {
+            author,
+            blogs: bMap[author]
+        }
+    })
+    return authorBlogCount.reduce((pv, cv) => pv.count > cv.count ? pv : cv, {})
+}
+
+const mostLikes = (blogs) => {
+    let blog = _.orderBy(blogs, ['likes'], ['des'])[0];
+    return ({ author: blog.author, likes: blog.likes })
+}
+
 
 module.exports = {
     dummy,
     totalLikes,
-    favoriteBlog
+    favoriteBlog,
+    mostBlogs,
+    mostLikes
 }
 
