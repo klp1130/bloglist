7282feefc1fb1723053ebc5192a504dfd72af1b6
Keenan Lee-Peters <keenan.leepeters@gmail.com>
2022-07-04 12:37:06 -0600

    initialize database bedore every test with beforeEach function

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -1,8 +1,19 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
-
 const api = supertest(app)
+const Blog = require('../models/blog')
+
+/* initializing the database before every
+ test with the beforeEach function */
+
+beforeEach(async () => {
+    await Blog.deleteMany({})
+    let blogObject = new Blog(initialBlogs[0])
+    await blogObject.save()
+    blogObject = new Blog(initialBlogs[1])
+    await blogObject.save()
+})
 
 test('blogs are returned as json', async () => {
     await api
