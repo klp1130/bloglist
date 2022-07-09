99f984dbd8dc9bf69e4468c577ab95ca3204f951
Keenan Lee-Peters <keenan.leepeters@gmail.com>
2022-07-09 16:10:51 -0600

    added controllers users post

---
+++ controllers/users.js
@@ -0,0 +1,23 @@
+const bycrypt = require('bcrypt')
+const { request, response } = require('../app')
+const usersRouter = require('express').Router()
+const User = require('../models/user')
+
+usersRouter.post('/', async (request, response) => {
+    const { username, name, password } = request.body
+
+    const saltRounds = 10
+    const passwordHash = await bycrypt.hash(password, saltRounds)
+
+    const user = new User({
+        username,
+        name,
+        passwordHash,
+    })
+
+    const savedUser = await user.save()
+
+    response.status(201).json(savedUser)
+})
+
+module.exports = usersRouter
