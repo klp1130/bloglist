b811c23814e4138dc0e66e632d110488dfafc2e7
Keenan Lee-Peters <96672628+klp1130@users.noreply.github.com>
2022-07-09 07:49:53 -0600

    added route handler for deletion of blogs by ID

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,4 +1,5 @@
 const blogsRouter = require('express').Router()
+
 const Blog = require('../models/blog')
 
 
@@ -25,6 +26,11 @@ blogsRouter.post('/', async (request, response) => {
 
     const savedBlog = await blog.save()
     response.json(savedBlog)
+})
+
+blogsRouter.delete('/:id', async (request, response) => {
+    await Blog.findByIdAndRemove(request.params.id)
+    response.status(204).end()
 })
 
 module.exports = blogsRouter
