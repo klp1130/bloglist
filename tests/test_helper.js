e65249960e82fd94782b88e3bab9841eef2d4f99
Keenan Lee-Peters <96672628+klp1130@users.noreply.github.com>
2022-07-10 08:16:15 -0600

    user test half done

--- tests/test_helper.js
+++ tests/test_helper.js
@@ -27,7 +27,7 @@ const blogsInDb = async () => {
 
 const usersInDb = async () => {
     const users = await User.find({})
-    return users.map(u => u.toJSON)
+    return users.map(u => u.toJSON())
 }
 
 
