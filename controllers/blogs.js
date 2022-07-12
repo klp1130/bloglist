b2113c50746ae702e3fe31421a9d62b86fec7c4f
Keenan Lee-Peters <keenan.leepeters@gmail.com>
2022-07-12 17:49:13 -0600

    added functionality for limiting creating new blogs to logged in users

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -3,16 +3,32 @@ const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
 
+const jwt = require('jsonwebtoken')
+
+
 blogsRouter.get('/', async (request, response) => {
     const blogs = await Blog
         .find({}).populate('user', { username: 1, name: 1 })
     response.json(blogs)
 })
 
+const getTokenFrom = request => {
+    const authorization = request.get('authorization')
+    if (authorization && authorization.toLowerCase().startsWith('bearer')){
+        return authorization.substring(7)
+    }
+    return null
+}
+
 blogsRouter.post('/', async (request, response) => {
     const body = request.body
+    const token = getTokenFrom(request)
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+    if (!decodedToken.id) {
+        return response.status(401).json({ error: 'token missing or invalid' })
+    }
 
-    const user = await User .findById(body.userId)
+    const user = await User .findById(decodedToken.Id)
 
     if (!request.body.url || !request.body.title) {
         return response.status(400).json({
